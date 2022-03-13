c358e5ef73386dabf4ce16a1a7b5d949792960e8
glenluersman <glen.luersman@gmail.com>
2022-03-13 11:56:08 -0400

    broken code

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,5 +1,5 @@
-import React, { useEffect } from 'react';
-import { capitalizeFirstLetter } from '../../utils/helpers';
+import React from "react";
+import { capitalizeFirstLetter } from "../../utils/helpers";
 
 function Nav(props) {
   const {
@@ -7,17 +7,18 @@ function Nav(props) {
     setCurrentCategory,
     currentCategory,
   } = props;
-  
-  useEffect(() => {
-    document.title = capitalizeFirstLetter(currentCategory.name);
-  }, [currentCategory]);
 
+
+  const handleClick = (item) => {
+    console.log(item);
+    return item;
+  };
 
   return (
-    <header className='flex-row px-1'>
+    <header className="flex-row px-1">
       <h2>
-        <a data-testid= "link" href="/">
-          <span role="img" aria-label="camera">📸</span> Oh Snap!
+        <a data-testid="link" href="/">
+          <span role="img" aria-label="camera"> 📸</span> Oh Snap!
         </a>
       </h2>
       <nav>
@@ -27,12 +28,23 @@ function Nav(props) {
               About me
             </a>
           </li>
-          <li className='mx-2'>
-            <span>Contact</span>
+          <li className={"mx-2"}>
+            <span onClick={() => handleClick('Contact')}>
+              Contact
+            </span>
           </li>
           {categories.map((category) => (
-            <li className={`mx-1 ${currentCategory.name === category.name && 'navActive'}`} key={category.name}>
-              <span onClick={() => { setCurrentCategory(category)}} >
+            <li
+              className={`mx-1 ${
+                currentCategory.name === category.name
+                }`}
+              key={category.name}
+            >
+              <span
+                onClick={() => {
+                  setCurrentCategory(category);
+                }}
+              >
                 {capitalizeFirstLetter(category.name)}
               </span>
             </li>
