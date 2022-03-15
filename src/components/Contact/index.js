a29551bbdf53c01c63a82c14e978e1ea38fdee5b
glenluersman <glen.luersman@gmail.com>
2022-03-15 19:36:36 -0400

    SPA and conditional styling

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,11 +1,32 @@
 import React, { useState } from 'react';
+import { validateEmail } from '../../utils/helpers';
 
 function ContactForm() {
   const [formState, setFormState] = useState({ name: '', email: '', message: '' });
   const { name, email, message } = formState;
+  const [errorMessage, setErrorMessage] = useState('');
 
   function handleChange(e) {
-    setFormState({...formState, [e.target.name]: e.target.value })
+    if (e.target.name === 'email') {
+      const isValid = validateEmail(e.target.value);
+      console.log(isValid);
+      // isValid conditional statement
+      if (!isValid) {
+        setErrorMessage('Your email is invalid!');
+      } else {
+        setErrorMessage('');
+      }
+    } else {
+      if (!e.target.value.length) {
+        setErrorMessage(`${e.target.name} is required!`);
+      } else {
+        setErrorMessage('');
+      }
+    }
+
+    if (!errorMessage) {
+      setFormState({...formState, [e.target.name]: e.target.value })
+    }
   }
 
   function handleSubmit(e) {
@@ -19,16 +40,21 @@ function ContactForm() {
       <form id="contact-form" onSubmit={handleSubmit}>
         <div>
           <label htmlFor='name'>Name:</label>
-          <input type="text" onChange={handleChange} name="name" defaultValue={name} />
+          <input type="text" onBlur={handleChange} name="name" defaultValue={name} />
         </div>
         <div>
           <label htmlFor='email'>Email address:</label>
-          <input type='text' onChange={handleChange} name='email' defaultValue={email} />
+          <input type='text' onBlur={handleChange} name='email' defaultValue={email} />
         </div>
         <div>
           <label htmlFor='message'>Message:</label>
-          <textarea name='massage' onChange={handleChange} defaultValue={message} rows='5' />
+          <textarea name='massage' onBlur={handleChange} defaultValue={message} rows='5' />
         </div>
+        {errorMessage && (
+          <div>
+            <p className='error-text'>{errorMessage}</p>
+          </div>
+        )}
         <button type='submit'>Submit</button>
       </form>
     </section>
